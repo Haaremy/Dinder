6df0f0a561dff9a8fdb02ae8332f71bc85bbfe95
stjmbeck <jeremypaul.becker@student.hs-anhalt.de>
2022-10-12 16:57:58 +0200

    Fixed Registration

--- app/src/main/java/com/hsa/finder/Register2.java
+++ app/src/main/java/com/hsa/finder/Register2.java
@@ -1,29 +1,59 @@
 package com.hsa.finder;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.neo4j.driver.Config;
 
 import testPack.DBCom;
 
 public class Register2 extends AppCompatActivity {
-
+    protected static String user;
+    protected static String clear;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+        EditText un = findViewById(R.id.username);
+        EditText cn = findViewById(R.id.clearname);
         findViewById(R.id.CreateProfile).setOnClickListener(view -> {
             finishReg();
         });
 
-    }
 
+
+    un.addTextChangedListener(new TextWatcher() { // watches Password
+
+        public void afterTextChanged(Editable s) {}
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        public void onTextChanged(CharSequence s, int start,
+        int before, int count) {
+
+            user = un.getText().toString();
+        }
+    });
+
+        cn.addTextChangedListener(new TextWatcher() { // watches Password
+
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+
+                clear = cn.getText().toString();
+            }
+        });
+    }
     public void finishReg() {
         // is Username taken?{}
         new MyTask().execute();
@@ -38,10 +68,8 @@ public class Register2 extends AppCompatActivity {
             String uri = "neo4j+s://0a1e255a.databases.neo4j.io:7687";
             String user = "neo4j";
             String psw= "dEn2QFo4_9d2Q0INYabLQzgqfXDP3fIJEQ4k_wWgO_A";
-            String username = "testuser1";
-            String clearname = "Max Mustermann";
             try (DBCom app = new DBCom(uri, user, psw, Config.defaultConfig())) {
-                app.createUser(username,Register1.email, Register1.password,clearname);
+                app.createUser(Register2.user,Register1.email, Register1.password,Register2.clear);
                 result = "true";
             } catch (Exception e) {
                 e.printStackTrace();
